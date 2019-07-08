e1dc9ddcd4199ed3d95640ad00872f9a62796554
Michael Hall <mohall42@students.tntech.edu>
2019-07-08 12:41:07 -0400

    Inital commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const IndexPg = () =>{
+
+    return (
+        <div>
+            <h1>Software Developer </h1>
+            <h2>CompSci grad with a passion for building for the web</h2>
+        </div>
+    )
+}
+
+export default IndexPg
